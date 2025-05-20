7ef95aa94abe5f14251e959822b63427bc8a6cd2
ASIF <f.asif.official@gmail.com>
2025-05-20 19:36:03 +0600

    v-0.00015

--- xApi/settings.py
+++ xApi/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
     'corsheaders',
-    'djstripe',
     'xApiAuthentication',
     'xApiProduct',
     'xApiCart',
